ab6ce6ac8e2a6c8c1264533b14dec026ceed3f13
qtycat <135958262+russolazy@users.noreply.github.com>
2025-04-06 14:26:57 +0800

    added loading screen and fixed the layout

--- src/app/components/display-pokemon-details/display-pokemon-details.component.ts
+++ src/app/components/display-pokemon-details/display-pokemon-details.component.ts
@@ -11,15 +11,22 @@ export class DisplayPokemonDetailsComponent implements OnChanges {
   constructor(private _pokemon: PokeApiService) {}
 
   pokemonDetails: any = {};
+  isLoading: boolean = false;
 
   ngOnChanges(): void {
     console.log('Display Component, pokemon:', this.pokemon);
+
+    this.isLoading = true;
 
     this._pokemon.getSpecificPokemonDetailsId(this.pokemon).subscribe(
       (res) => {
         console.log('API Response: ', res);
 
         this.pokemonDetails = res;
+
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 3750);
       },
       (err) => {
         console.error('API Error: ', err);
